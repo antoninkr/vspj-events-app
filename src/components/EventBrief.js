d19224e81bd3cdc69c1c1659a0de9f6e06acc4c2
Antonín Kroupa <antoninkroupa@email.cz>
2023-04-25 17:25:41 +0200

    Fixed date from date reload

--- src/components/EventBrief.js
+++ src/components/EventBrief.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import moment from 'moment-timezone';
+import Moment from 'react-moment';
 
 const EventBrief = ({ event, onPress }) => {
   const { startAt, endAt } = event;
@@ -8,7 +9,8 @@ const EventBrief = ({ event, onPress }) => {
   const endAtMoment = moment(endAt);
   const fromDate = new Date(); // 2022, 0, 14, 12, 33
   // const fromDate = new Date();
-  let fromNow = startAtMoment.from(fromDate);
+  //let fromNow = startAtMoment.from(fromDate);
+  let fromNow = startAtMoment;
   let start = true;
 
   if (startAt.getTime() <= fromDate.getTime()) {
@@ -17,17 +19,23 @@ const EventBrief = ({ event, onPress }) => {
     //   'MM.DD.YY'
     // )}`;
     if (endAtMoment.unix()) {
-      fromNow = endAtMoment.from(fromDate);
-    } else {
+      // fromNow = endAtMoment.from(fromDate);
+      fromNow = endAtMoment;
+    }
+    /*
+    else {
       fromNow = startAtMoment.format('MM.DD.YYYY');
     }
+    */
   }
+
+  const fromNowJSX = <Moment date={fromNow} element={Text} fromNow />; //fromNow
 
   return (
     <TouchableOpacity onPress={onPress}>
       <View style={styles.container}>
         <Text style={styles.title}>{event.title.replace(/&quot;/g, '"')}</Text>
-        <Text style={styles.time}>{start ? fromNow : 'Probíhá'}</Text>
+        <Text style={styles.time}>{start ? fromNowJSX : 'Probíhá'}</Text>
       </View>
     </TouchableOpacity>
   );
